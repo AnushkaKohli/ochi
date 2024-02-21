c116f0e12f4243af9bbf59bbf24f7fec7b12e17b
Anushka <anushkakohli2021@gmail.com>
2024-02-21 14:54:09 +0530

    marquee

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,15 +1,34 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Marquee = () => {
   return (
-    <div className="w-full rounded-t-2xl py-20 bg-[#004d43] text-[#e7e7e7]">
+    <div className="w-full rounded-t-2xl py-[6vw] bg-[#004d43] text-[#e7e7e7]">
       <div className="text border-t-[1px] border-b-[1px] border-zinc-300 flex gap-10 overflow-hidden whitespace-nowrap">
-        <h1 className="text-[20vw] leading-none font-foundersgrotesk uppercase -mb-20 pt-5">
+        <motion.h1
+          initial={{ x: 0 }}
+          animate={{ x: "-100%" }}
+          transition={{ ease: "linear", repeat: Infinity, duration: 5 }}
+          className="text-[20vw] leading-none font-foundersgrotesk uppercase -mb-[5vw] pt-5"
+        >
           we are ochi
-        </h1>
-        <h1 className="text-[20vw] leading-none font-foundersgrotesk uppercase -mb-20 pt-5">
+        </motion.h1>
+        <motion.h1
+          initial={{ x: 0 }}
+          animate={{ x: "-100%" }}
+          transition={{ ease: "linear", repeat: Infinity, duration: 5 }}
+          className="text-[20vw] leading-none font-foundersgrotesk uppercase -mb-[5vw] pt-5"
+        >
           we are ochi
-        </h1>
+        </motion.h1>
+        <motion.h1
+          initial={{ x: 0 }}
+          animate={{ x: "-100%" }}
+          transition={{ ease: "linear", repeat: Infinity, duration: 5 }}
+          className="text-[20vw] leading-none font-foundersgrotesk uppercase -mb-[5vw] pt-5"
+        >
+          we are ochi
+        </motion.h1>
       </div>
     </div>
   );
