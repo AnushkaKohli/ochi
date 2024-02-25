9c2dd60f07b54645ade903e24d01fff57ac2c0b0
Anushka <anushkakohli2021@gmail.com>
2024-02-25 14:12:37 +0530

    word animation

--- src/components/FeaturedCard.jsx
+++ src/components/FeaturedCard.jsx
@@ -1,31 +1,57 @@
 import React from "react";
+import { motion, useAnimation } from "framer-motion";
 
 const FeaturedCard = (props) => {
   const { title, image, link, buttons, direction } = props;
+  const animatedCard = useAnimation();
+  const handleHoverStart = () => {
+    animatedCard.start({
+      y: "0%",
+    });
+  };
+  const handleHoverEnd = () => {
+    animatedCard.start({
+      y: "100%",
+    });
+  };
   return (
     <div className="featuredCard">
+      {/* Title */}
       <div className="uppercase flex items-center text-sm mb-4">
         <span className="w-3 h-3 flex-shrink-0 rounded-full bg-black dark:bg-zinc-100 mr-6"></span>
         <span className="font-medium tracking-tight text-lg">{title}</span>
       </div>
-      <a className="cardContainer mb-6 relative block" href={link}>
+      {/* Image Card */}
+      <motion.a
+        onHoverStart={() => handleHoverStart()}
+        onHoverEnd={() => handleHoverEnd()}
+        className="cardContainer mb-6 relative block"
+        href={link}
+      >
         <h1
-          className={`headline ${
-            direction === "left" ? "left-full" : "left-0"
-          } absolute z-[9] leading-none tracking-tight text-8xl top-1/2 -translate-x-1/2 -translate-y-1/2 z-2 text-[#cdea68] uppercase font-semibold font-foundersgrotesk whitespace-nowrap`}
+          className={`headline left-1/2 ${
+            direction === "left" ? "md:left-full" : "md:left-0 "
+          } absolute z-[9] leading-none tracking-tight text-8xl top-1/2 -translate-x-1/2 -translate-y-1/2 z-2 text-[#cdea68] uppercase font-semibold font-foundersgrotesk whitespace-nowrap flex overflow-hidden`}
         >
           {title.split("").map((alphabet, index) => {
             return (
-              <span key={index} className="">
+              <motion.span
+                initial={{ y: "100%" }}
+                animate={animatedCard}
+                transition={{ ease: [0.22, 1, 0.36, 1], delay: index * 0.05 }}
+                key={index}
+                className="iniline-block"
+              >
                 {alphabet}
-              </span>
+              </motion.span>
             );
           })}
         </h1>
         <div className="cardImage rounded-xl w-full h-full overflow-hidden">
           <img className="w-full h-full bg-cover" src={image} alt={title} />
         </div>
-      </a>
+      </motion.a>
+      {/* Buttons */}
       <div className="buttonContainer flex flex-wrap -mb-10 items-start">
         {buttons.map((button, index) => {
           return (
